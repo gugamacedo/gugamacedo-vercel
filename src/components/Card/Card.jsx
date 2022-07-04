402b07486a2207faec248edd7aa570cd17728fee
Guga Macedo <94029266+gugamacedo@users.noreply.github.com>
2022-07-04 13:19:43 -0300

    Update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,28 +1,31 @@
 import style from './Card.module.css'
 
-import data from "../../data.json"
-
-const parse = require('html-react-parser')
+import cards from "../../data"
 
 export function Card() {
-
   return (
     <main className={style.main}>
       <ul className={style.cards}>
         {
-          data.map(item => (
-            <li className={style.card} key={item.title}>
-            <h3>{item.title}</h3>
-            <a href={item.link} target="_blank" rel="noreferrer">
-              <img src={item.image} alt={item.title} />
+          cards.map(card => (
+            <li className={style.card} key={card.title}>
+            <h3>{card.title}</h3>
+            <a href={card.link} target="_blank" rel="noreferrer">
+              <img src={card.image} alt={card.title} />
             </a>
             <div>
               <p>
-                <b>About:</b> {item.about}
+                <b>About:</b> {card.about}
               </p>
               <p className='made'>
                 <b>Made with: </b>
-                {parse(item.made)}
+                {card.made.map((item) => (
+                    <img
+                      key={item.name}
+                      src={`https://img.shields.io/badge/${item.name}-8257e6.svg?style=for-the-badge&logo=${item.logo}&logoColor=white`}
+                      alt={item.name}
+                    />
+                  ))}
               </p>
             </div>
           </li>
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    title: 'Dunder Mifflin',
+    link: 'https://dunder-mifflin.vercel.app/',
+    image: '/dunder-mifflin.png',
+    about:
+      "SSG page for the best sitcom ever! You can view the company's history, file a complaint, view our team and details about each one.",
+    made: [
+      { name: 'NextJS', logo: 'next.js' },
+      { name: 'React', logo: 'react' },
+      { name: 'Styled--Components', logo: 'styledcomponents' },
+    ],
+  },
+
+  {
+    title: 'React QrCode',
+    link: 'https://react-qr-code.vercel.app',
+    image: '/react-qr-code.png',
+    about:
+      'Insert the link, choose the color and background color and generate a custom QR Code! You can also download the QRCode image.',
+    made: [
+      { name: 'React', logo: 'react' },
+      { name: 'Material.UI', logo: 'mui' },
+    ],
+  },
+
+  {
+    title: 'Dev.Finance$',
+    link: 'ttps://gugamacedo.github.io/dev-finances/',
+    image: '/dev-finances.png',
+    about:
+      'Control your finances! User can add, edit or delete a profit/expense and     generate balance. Also with dark mode!',
+    made: [
+      { name: 'Javascript', logo: 'javascript' },
+      { name: 'SASS', logo: 'sass' },
+    ],
+  },
+
+  {
+    title: 'Luxury Dealership',
+    link: 'https://gugamacedo.github.io/luxury-dealership/',
+    image: '/luxury-dealership.png',
+    about:
+      'Parallax site for a fictional store selling used luxury vehicles. See vehicle categories and their owners, and also register an offer for any of the vehicles.',
+    made: [
+      { name: 'HTML', logo: 'html5' },
+      { name: 'CSS', logo: 'css3' },
+    ],
+  },
+]
+
+export default data
