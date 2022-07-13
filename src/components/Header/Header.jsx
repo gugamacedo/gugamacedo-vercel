e96f045cb1625041073f342e3e27c0e3c678cc28
Guga Macedo <94029266+gugamacedo@users.noreply.github.com>
2022-07-13 00:12:37 -0300

    feat: design improvement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,21 @@ export function Header() {
       <div className={style.avatar}>
         <img src="/avatar_small.png" alt="Guga Macedo" />
         <h1>Guga Macedo</h1>
-        <h2>FullStack Developer</h2>
+        <h2>Frontend Developer</h2>
       </div>
 
       <div className={style.links}>
         <li>
           <a href="https://github.com/gugamacedo" target="_blank" rel="noreferrer">
             <img src="/github.svg" alt="Github Profile" />
+            <h4>Github</h4>
           </a>
         </li>
 
         <li>
           <a href="https://linkedin.com/in/gugamacedo" target="_blank" rel="noreferrer">
             <img src="./linkedin.svg" alt="Linkedin Profile" />
+            <h4>Linkedin</h4>
           </a>
         </li>
       </div>
